b7a54436617f249129342d57b3bc9b935fcf8bec
dwipapapa <83564308+dwipapapa@users.noreply.github.com>
2021-05-11 12:21:46 +0700

    Added Boss behaviours but only two for now

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 namespace SpriteKind {
     export const Big_Bad_Boss = SpriteKind.create()
     export const Boss_Projectile = SpriteKind.create()
+    export const Poison_Projectile = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.Poison_Projectile, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.destroy()
+    for (let index = 0; index < randint(2, 10); index++) {
+        timer.after(1000, function () {
+            info.changeLifeBy(-1)
+        })
+    }
+})
 function make_big_bad_boss () {
     Big_Bad_boss_health = 100
     Boss = sprites.create(Big_Bad_Boss_Images.shift(), SpriteKind.Big_Bad_Boss)
@@ -176,20 +185,184 @@ statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     character.setCharacterState(thePlayer, character.rule(Predicate.FacingRight, Predicate.MovingRight))
 })
-scene.onOverlapTile(SpriteKind.Big_Bad_Boss, assets.tile`myTile1`, function (sprite, location) {
-    sprite.vy = 25
-    boss_shoot()
-})
-function boss_shoot () {
-    for (let index = 0; index < 16; index++) {
-        projectile2 = sprites.createProjectileFromSprite(img`
-            d d 
-            d d 
-            `, Boss, -120, 0)
-        projectile2.setKind(SpriteKind.Boss_Projectile)
-        projectile2.y += randint(-16, 16)
+function boss_Attack () {
+    if (Boss.image.equals(img`
+        . . . . . . . . . . . . . . e e 
+        . . . . . . . . . . . . . e e e 
+        . . . . . . . . . . . . e e 2 2 
+        . . . . . . . . . . . . e 2 2 4 
+        . . . . . . . . . . . e f 4 4 2 
+        . . . . . . . . . . e e f 2 2 2 
+        . . . . . . . . e e e 4 e 2 2 2 
+        d d d b f 2 f 2 4 4 e 2 e 2 2 2 
+        c c c c f c f e e 2 c 2 c 2 e e 
+        . . . . . . . . e e c 2 c e e e 
+        . . . . . . . . . . c e f e c c 
+        . . . . . . . . . . . e f c c c 
+        . . . . . . . . . . . . f c c c 
+        . . . . . . . . . . . . c c c c 
+        . . . . . . . . . . . . . c c c 
+        . . . . . . . . . . . . . . c c 
+        `) || Boss.image.equals(img`
+        ........................
+        ........................
+        ..........ccc...........
+        .........cccc...........
+        ...11111111c..ccc.......
+        ..1111111111ccccc.......
+        1111111111111bcc........
+        11115555111115b..cc.....
+        1155551ff111155bccc.....
+        155d55ff51111555bc......
+        15555555511115555b......
+        1cbb31bb51111d555b.cc...
+        1c5333b551111dd55dccc...
+        11533b551111ddddddb.....
+        .11555dd11115bdddddccc..
+        ..11111111155bdddddccc..
+        ...111111555bddddddcc...
+        ....ccbc55bc5ddddddbcccc
+        .....cbbcc5555dddddddddc
+        .....ccbbb555ddbddddddc.
+        .....cdcbc55ddbbbdddcc..
+        ...ccdddccddddbcbbcc....
+        ...ccccccd555ddccc......
+        ........cccccccc........
+        `)) {
+        for (let index = 0; index < 16; index++) {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                d d 
+                d d 
+                `, Boss, -120, 0)
+            projectile2.setKind(SpriteKind.Boss_Projectile)
+            projectile2.y += randint(-16, 16)
+        }
+    } else if (Boss.image.equals(img`
+        . . . . f f f f . . . . 
+        . . f f 1 1 1 1 f f . . 
+        . f b 1 1 1 1 1 1 b f . 
+        . f 1 1 1 1 1 1 1 d f . 
+        f d 1 1 1 1 1 1 1 d d f 
+        f d 1 1 1 1 1 1 d d d f 
+        f d 1 1 1 d d d d d d f 
+        f d 1 d f b d d d d b f 
+        f b d d f c d b b b c f 
+        . f 1 1 1 1 1 b b c f . 
+        . f 1 b 1 f f f f f . . 
+        . f b f c 1 1 1 b f . . 
+        . . f f 1 b 1 b f f . . 
+        . . . f b f b f f f . f 
+        . . . . f f f f f f f f 
+        . . . . . . f f f f f . 
+        `)) {
+        for (let index = 0; index < 4; index++) {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                1 1 . . . 
+                1 1 . . . 
+                . . 1 . . 
+                . . . 1 1 
+                . . . 1 1 
+                `, Boss, -120, 0)
+            projectile2.setKind(SpriteKind.Boss_Projectile)
+            projectile2.y += randint(-16, 16)
+            effects.blizzard.startScreenEffect(100)
+        }
+    } else if (Boss.image.equals(img`
+        . . . . f f f f f f f f . . . . 
+        . . f f f f f f f f f f f f . . 
+        . f f f f f f f f f f f f f f . 
+        . f f f f f f f f f f f f f f . 
+        f f 2 2 f f f f f f f 2 2 f f f 
+        f f f f 2 2 f f f 2 2 f f f f f 
+        f f f f f f 2 2 2 f f f f f f f 
+        f f f 2 2 f f f f f 2 2 f f f f 
+        f f f 2 2 f f f f f 2 2 f f f f 
+        f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f 
+        f f 2 2 f f f f f f f 2 2 f f f 
+        . f f f 2 2 f f f 2 2 f f f f f 
+        . f f f f f 2 2 2 f f f f f f . 
+        . . f f f f f f f f f f f f . . 
+        . . . . f f f f f f f f . . . . 
+        `)) {
+        for (let index = 0; index < 4; index++) {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                f f 
+                f f 
+                `, Boss, -120, 0)
+            projectile2.setKind(SpriteKind.Boss_Projectile)
+            projectile2.y += randint(-16, 16)
+        }
+    } else if (Boss.image.equals(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 7 . . . 7 . . . . . 
+        . . . . . . 7 7 . 7 7 . . . . . 
+        . . . . . . 7 7 7 7 7 . . . . . 
+        . . . . . . 7 2 7 2 7 . . . . . 
+        . . . . . . 7 7 7 7 7 . . . . . 
+        . . . . . . 7 f f f 7 . . . . . 
+        . . . . . . . 7 7 7 . . . . . . 
+        . . . . . . 7 . 7 . 7 . . . . . 
+        . . . . . 7 . . 7 . . 7 . . . . 
+        . . . . 7 . . . 7 . . . 7 . . . 
+        . . . . . . . . 7 . . . . . . . 
+        . . . . . . . . 7 . . . . . . . 
+        . . . . . . . . 7 . . . . . . . 
+        . . . . . . . 7 . 7 . . . . . . 
+        . . . . . . 7 . . . 7 . . . . . 
+        `)) {
+        for (let index = 0; index < 4; index++) {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                7 . . . . 
+                . 7 . . . 
+                . . 7 . . 
+                . . . 7 . 
+                . . . . 7 
+                `, Boss, -120, 0)
+            projectile2.setKind(SpriteKind.Boss_Projectile)
+            projectile2.y += randint(-16, 16)
+        }
+        animation.runMovementAnimation(
+        Boss,
+        animation.animationPresets(animation.bobbing),
+        1000,
+        false
+        )
+    } else if (Boss.image.equals(img`
+        . . . . . . . c c . . . . . . . 
+        . . . . . . c 7 c . . . . . . . 
+        . . . . c c 7 7 7 c c c . . . . 
+        . . c c c c 7 7 7 7 c 6 c c . . 
+        . c 6 6 7 6 7 7 7 7 6 7 6 6 c . 
+        . c 6 7 7 6 6 7 7 6 6 7 7 6 c . 
+        . . c 7 7 7 6 6 6 6 7 7 7 f . . 
+        . . . f 7 7 7 7 7 7 7 7 f f . . 
+        . . . . f 7 7 7 f b 7 7 f f . . 
+        . . . . f 7 6 6 f 1 6 f f f . . 
+        . . . . f 6 6 6 6 6 6 f f . . . 
+        . . . . . f 7 7 7 7 f 7 7 . . . 
+        . . . . . f 7 6 6 7 6 6 7 . . . 
+        . . . . f 7 7 7 7 7 6 6 7 . . . 
+        . . . . c 6 7 7 7 7 7 7 . . . . 
+        . . . . . f f f f f f . . . . . 
+        `)) {
+        for (let index = 0; index < 8; index++) {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . 1 1 1 
+                . 1 1 1 1 
+                1 1 1 1 1 
+                1 1 . . . 
+                1 . . . . 
+                `, Boss, -120, 0)
+            projectile2.setKind(SpriteKind.Poison_Projectile)
+            projectile2.y += randint(-16, 16)
+        }
     }
 }
+scene.onOverlapTile(SpriteKind.Big_Bad_Boss, assets.tile`myTile1`, function (sprite, location) {
+    sprite.vy = 25
+    boss_Attack()
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Big_Bad_Boss, function (sprite, otherSprite) {
     sprite.destroy()
     Big_Bad_boss_health += -1
@@ -198,7 +371,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Big_Bad_Boss, function (spri
 scene.onOverlapTile(SpriteKind.Big_Bad_Boss, assets.tile`myTile0`, function (sprite, location) {
     sprite.vy = -25
     timer.after(randint(500, 3000), function () {
-        boss_shoot()
+        boss_Attack()
     })
 })
 let projectile: Sprite = null
